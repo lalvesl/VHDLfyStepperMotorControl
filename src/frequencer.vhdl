2b1f09bceb004e9303c756b3497e972c29600734
lalvesl <alvesdelima.lucas45@gmail.com>
2023-06-22 17:18:12 -0300

    Criado architecture para auto divisão de frequências.

--- src/frequencer.vhdl
+++ src/frequencer.vhdl
@@ -5,26 +5,26 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY frequencer IS
     PORT (
-        frequencer_clk_pin : IN BIT;
+        clk_in : IN BIT;
         frequencyIn : IN NATURAL;
         frequencyOut : IN NATURAL;
-        clk_out : OUT STD_LOGIC
+        clk_out : OUT BIT
     );
 END frequencer;
 
 ARCHITECTURE afrequencer OF frequencer IS
-    SIGNAL counter : NATURAL RANGE 0 TO frequencyIn;
-    SIGNAL state : STD_LOGIC := '0';
+    SIGNAL counter : NATURAL RANGE 0 TO frequencyIn := 0;
+    SIGNAL state : BIT := '0';
 BEGIN
-    pfrequencer : PROCESS (frequencer_clk_pin, frequencyIn, frequencyOut)
+    pfrequencer : PROCESS (clk_in, frequencyIn, frequencyOut)
     BEGIN
-        IF (frequencer_clk_pin'event AND frequencer_clk_pin = '1') THEN
-            IF counter < (frequencyIn/frequencyOut) THEN
+        IF (clk_in'event) THEN
+            IF counter < ((frequencyIn/frequencyOut) - 1) THEN
                 counter <= counter + 1;
             ELSE
                 counter <= 0;
+                clk_out <= NOT state;
                 state <= NOT state;
-                clk_out <= state;
             END IF;
         END IF;
     END PROCESS;
