51e4aabba3e46e010776a8ba0f156f8b3ec9b10b
lalvesl <alvesdelima.lucas45@gmail.com>
2023-06-26 13:29:11 -0300

    Removido código desnecessário.

--- src/configuration.vhdl
+++ src/configuration.vhdl
@@ -1,11 +1,4 @@
--- PACKAGE std_configs IS
---     TYPE std_config IS RECORD
---         clk_in : BIT;
---         frequencyStd : NATURAL;
---     END RECORD;
--- END PACKAGE CONFIGURATION;
 PACKAGE std_configs IS
-    -- Define the record type
     TYPE std_config IS RECORD
         clk_in : BIT;
         frequencyStd : NATURAL;
