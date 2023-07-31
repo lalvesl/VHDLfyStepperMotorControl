ed945cb6f499dded225627611541f76b6de67d40
lalvesl <alvesdelima.lucas45@gmail.com>
2023-07-31 09:53:16 -0300

    Adicionado arquivo de aliases de integração com o Quartus.

---
+++ quartusFiles/aliasesQuartus.sh
@@ -0,0 +1,10 @@
+export ALTERAPATH="/opt/quartus/altera/14.0/"
+export ALTERAOCLSDKROOT="${ALTERAPATH}/hld"
+export QUARTUS_ROOTDIR=${ALTERAPATH}/quartus
+export QUARTUS_ROOTDIR_OVERRIDE="$QUARTUS_ROOTDIR"
+export PATH=$PATH:${ALTERAPATH}/quartus/bin
+export PATH=$PATH:${ALTERAPATH}/nios2eds/bin
+export PATH=$PATH:${QSYS_ROOTDIR}
+
+
+
